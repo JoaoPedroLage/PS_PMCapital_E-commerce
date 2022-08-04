b5c8e4a17c65bd96b43bb5f29ec4a501240cd5f5
JoaoPedroLage <joaopedro.lage@outlook.com>
2022-08-04 13:34:23 -0300

    fix: correct wrong value for price migration and model for data base e-commerce

--- back-end/src/database/seeders/20220803220900-tickets.js
+++ back-end/src/database/seeders/20220803220900-tickets.js
@@ -6,53 +6,53 @@ module.exports = {
       {
         id: 1,
         name: 'ingresso rock in rio 2022',
-        price: 312,
+        price: 312.00,
       },
       {
         id: 2,
         name: 'ingresso show L7',
-        price: 50,
+        price: 50.00,
       },
       {
         id: 3,
         name: 'ingresso Baile da Arena',
-        price: 110,
+        price: 110.00,
       },
       {
         id: 4,
         name: 'ingresso os melhores do Funk',
-        price: 75,
+        price: 75.00,
       },
       {
         id: 5,
         name: 'ingresso Planeta Brasil',
-        price: 200,
+        price: 200.00,
       },
       {
         id: 6,
         name: 'ingresso Rave Universo Paralello',
-        price: 449,
+        price: 449.99,
       },
       {
         id: 7,
         name: 'ingresso show Lana Del Rey',
-        price: 499,
+        price: 499.99,
       
       },
       {
         id: 8,
         name: 'ingresso So Track Boa',
-        price: 279,
+        price: 279.99,
       },
       {
         id: 9,
         name: 'ingresso area vip show Lady Gaga',
-        price: 889,
+        price: 889.00,
       },
       {
         id: 10,
         name: 'ingresso area vip Festival Sertanejo',
-        price: 350,
+        price: 350.00,
       },
     ],
     {},
