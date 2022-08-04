d0c4a91be65d2c7e2073797a61e66e9eb460e0ac
JoaoPedroLage <joaopedro.lage@outlook.com>
2022-08-04 11:59:19 -0300

    fix: login background image

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,9 +1,11 @@
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable import/no-named-as-default-member */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 import AppProvider from './context/AppProvider';
 
 ReactDOM.render(
